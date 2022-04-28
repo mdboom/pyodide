a2a0ca1ac59713815d74944d57e424e3882f9273
Michael Droettboom <mdboom@gmail.com>
2022-04-28 11:11:58 -0400

    Fix #2422: Check for cmake (req. by emsdk) and libtool (req. by libffi)

--- tools/dependency-check.sh
+++ tools/dependency-check.sh
@@ -36,6 +36,14 @@ check_shasum() {
   check_binary_present "shasum"
 }
 
+check_cmake() {
+  check_binary_present "cmake"
+}
+
+check_libtool() {
+  check_binary_present "libtool"
+}
+
 check_fortran_dependencies() {
   check_binary_present "gfortran"
   check_binary_present "f2c"
@@ -43,6 +51,8 @@ check_fortran_dependencies() {
 
 check_python_version
 check_pkgconfig
+check_cmake
+check_libtool
 #check_python_headers
 check_fortran_dependencies
 check_shasum
